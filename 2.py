ce3b33b428ed81d386661e0f45907bad7ca2a237
mrgick <45306973+mrgick@users.noreply.github.com>
2022-03-23 19:09:40 +0300

    add 2 lab

---
+++ 2.py
@@ -0,0 +1,47 @@
+"""
+2.
+Ввести произвольную строку в консоль.
+Циклом пройтись по всем символам этой строки
+Пропустить 3-й символ.
+Если есть символ "c" вывести сообщение об этом.
+Загуглить функцию вывода длины строки.
+Не выводить последний символ в строке.
+"""
+from duckduckgo_search import ddg
+
+
+def googling(keywords: str):
+    try:
+        res = ddg(keywords)
+        if len(res) > 0:
+            return res[0]
+        else:
+            return None
+    except:
+        return None
+
+
+def main():
+    input_value = input('Введите произвольную строку: ')
+    for i in range(0, len(input_value), 1):
+        if i == 3:
+            continue
+        if input_value[i] in ["c", "с"]:
+            print('В произвольной строке присутствует символ "c".')
+
+    print("\nМы гуглим")
+    result = googling("python str length")
+
+    if result:
+        print('\nВот, что мы нашли:')
+        print('Название:', result['title'])
+        print('Ссылка:', result['href'])
+        print('Информация:',result['body'],'\n')
+    else:
+        print('Гуглинг не удался...')
+
+    print('Произвольная строка без послденего символа:', input_value[:-1])
+
+
+if __name__ == "__main__":
+    main()
