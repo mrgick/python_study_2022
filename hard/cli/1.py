3ef6019d36032c1ae3d060dbce921c5c0ec08a4e
mrgick <45306973+mrgick@users.noreply.github.com>
2022-03-24 19:16:52 +0300

    done lab 10 "яма"

---
+++ hard/cli/1.py
@@ -0,0 +1,52 @@
+"""
+input содержит корректное
+приглашение для ввода;
+
+Задача 10. Яма
+
+Что нужно сделать
+
+Вы пишите компьютерную игру с
+текстовой графикой, вам поручили
+написать генеретор ландшафта,
+
+Напишите программу которая получает на
+вход число N и выводит на экран числа в
+сиде "ямы" вот так:
+
+5
+5........5
+54......45
+543....345
+5432..2345
+5432112345
+"""
+
+
+def main():
+    N = int(input())
+
+    if N <= 0:
+        return
+
+    max_dots = 0
+    for i in range(1, N + 1, 1):
+        max_dots += len(str(i))
+    max_dots *= 2
+
+    msg = ""
+    for i in range(1, N + 1, 1):
+        normal_line = ''
+        reversed_line = ''
+        for j in range(N, i - 1, -1):
+            tmp = str(j)
+            normal_line = normal_line + tmp
+            reversed_line = tmp + reversed_line
+        dots = '.' * (max_dots - len(normal_line) * 2)
+        msg = normal_line + dots + reversed_line + '\n' + msg
+
+    print(msg, end='')
+
+
+if __name__ == '__main__':
+    main()
