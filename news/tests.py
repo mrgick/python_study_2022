7923367828869626adfb162eb1532101c3332f42
mrgick <45306973+mrgick@users.noreply.github.com>
2022-04-03 13:21:09 +0300

    add test add and edit news

--- news/tests.py
+++ news/tests.py
@@ -47,19 +47,30 @@ class NewsAddEditDeleteViewsTestCase(TestCase):
         self.user = User.objects.create_superuser(username='test',
                                                   password='test')
         self.news_kwargs = {
-            'title': 'test',
-            'text': 'test',
+            'title': 'test1',
+            'text': 'test1',
             'blog': self.blog,
             'owner': self.user
+        }
+        self.news_form_data = {
+            'title': 'test2',
+            'text': 'test2',
+            'blog': str(self.blog.id),
+            'owner': str(self.user.id)
         }
         self.client.login(username='test', password='test')
 
     def doCleanup(self):
+
+        def delete_news(title):
+            news = News.objects.filter(title=title).first
+            if news:
+                news.delete()
+
         self.blog.delete()
         self.user.delete()
-        news = News.objects.filter(title=self.news_kwargs['title']).first
-        if news:
-            news.delete()
+        delete_news(self.news_kwargs['title'])
+        delete_news(self.news_form_data['title'])
 
     def get_page(self, url: str, status_code: int, error_message: str):
         response = self.client.get(url)
@@ -71,3 +82,22 @@ class NewsAddEditDeleteViewsTestCase(TestCase):
         self.assertEqual(response.status_code, 302, "Can't delete news.")
         news = News.objects.filter(id=news.id).first()
         self.assertIsNone(news, 'Error in sending data on news delete page.')
+
+    def test_news_add(self):
+        url = '/news/add/'
+        self.get_page(url, 200, "Can't get news add page.")
+        response = self.client.post(url, self.news_form_data)
+        self.assertEqual(response.status_code, 302, "Can't add news.")
+        news = News.objects.filter(title=self.news_form_data['title']).first()
+        self.assertIsNotNone(news, 'Error in sending data on news add page.')
+        news.delete()
+
+    def test_news_edit(self):
+        news = News.objects.create(**self.news_kwargs)
+        url = '/news/{}/edit/'.format(news.id)
+        self.get_page(url, 200, "Can't get news edit page.")
+        response = self.client.post(url, self.news_form_data)
+        self.assertEqual(response.status_code, 302, "Can't add news.")
+        news = News.objects.filter(title=self.news_form_data['title']).first()
+        self.assertIsNotNone(news, 'Error in sending data on news edit page.')
+        news.delete()
